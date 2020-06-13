2c2572d5f696493ca42435a89b1c50c5f11b3c04
Gökten Karadağ <gktnkrdg@gmail.com>
2020-06-14 01:27:08 +0300

    add word controller,check word exist and derive

---
+++ src/API/WordExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace KelimeBul.API
+{
+    public static class WordExtension
+    {
+        //https://stackoverflow.com/questions/41076904/check-if-chars-of-a-string-contains-in-another-string-with-linq
+        public static bool CanBeMadeFromLetters(this string word, string tileLetters)
+        {
+
+            var tileLetterCounts = GetLetterCounts(tileLetters);
+            var wordLetterCounts = GetLetterCounts(word);
+
+            return wordLetterCounts.All(letter =>
+                tileLetterCounts.ContainsKey(letter.Key)
+                && tileLetterCounts[letter.Key] >= letter.Value);
+        }
+
+        //Gets dictionary of letter/# of letter in word
+        public static Dictionary<char, int> GetLetterCounts(string word)
+        {
+            return word
+                .GroupBy(c => c)
+                .ToDictionary(
+                    grp => grp.Key,
+                    grp => grp.Count());
+        }
+    }
+}
