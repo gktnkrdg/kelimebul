8591abe025ef7b06159413b521b2a46a978304f0
Gökten Karadağ <gktnkrdg@gmail.com>
2020-06-14 10:03:00 +0300

    project structure change, tr dataset update

---
+++ src/KelimeBul.Web/ClientApp/src/components/Home.js
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+import { Nav, NavItem, NavLink, Button } from 'reactstrap';
+import history from './history';
+export class Home extends Component {
+    static displayName = Home.name;
+    constructor(props) {
+        super(props);
+
+        this.incrementCounter = this.incrementCounter.bind(this);
+    }
+
+    incrementCounter() {
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>KELİME BUL</h1>
+                <Button onClick={() => this.props.history.replace('/Counter') } color="success">Oyuna Başla</Button>
+               
+            </div>
+        );
+    }
+}
