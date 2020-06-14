a4c5ff4b1b9be930171cfa01daa8d84f261d4299
Gökten Karadağ <gktnkrdg@gmail.com>
2020-06-14 05:50:51 +0300

    fix read file linux container

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KelimeBul.API.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,10 +27,12 @@ namespace KelimeBul.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<ITurkishDictionary, TurkishDictionary>();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ITurkishDictionary turkishDictionary)
         {
             if (env.IsDevelopment())
             {
@@ -46,7 +49,7 @@ namespace KelimeBul.API
             {
                 endpoints.MapControllers();
             });
-            TurkishDictionary.ReadTurkishWordsFile();
+            turkishDictionary.ReadTurkishWordsFile();
         }
     }
 }
